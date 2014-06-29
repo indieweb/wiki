8c1cc6cec3a717c89f2648515703fcf4c9a93420
Peat Bakke <peat@peat.org>
2014-06-29 11:50:54 -0700

    Cleaning up sponsors

--- skins/sponsors.php
+++ skins/sponsors.php
@@ -1,32 +1,16 @@
-<style type="text/css">
-.sponsor-logo-block .logo {
-	float: left;
-	margin: 10px;
-	height: 70px;
-}
-.sponsor-logo-block .logo div {
-	padding: 6px;
-	background-color: #f2f2f2;
-	-webkit-border-radius: 4px;
-	-moz-border-radius: 4px;
-	border-radius: 4px;	
-}
-</style>
-<div class="sponsor-logo-block" style="background-color: #c9c9c9; padding: 30px;">
-	<div class="logo"><div><a href="https://www.brid.gy/"><img src="/sponsor-logos/bridgy.png" alt="Bridgy"></a></div></div>
-	<div class="logo"><div><a href="http://mozilla.org/"><img src="/sponsor-logos/mozilla.png" alt="Mozilla"></a></div></div>
-	<div class="logo"><div><a href="http://idno.co/"><img src="/sponsor-logos/idno.png" alt="Idno"></a></div></div>
-	<div class="logo"><div><a href="http://indiewebcamp.com/p3k"><img src="/sponsor-logos/p3k.png" alt="p3k"></a></div></div>
-	<div class="logo"><div><a href="http://google.com"><img src="/sponsor-logos/google.png" alt="Google"></a></div></div>
-	<div class="logo"><div><a href="http://superfeedr.com/"><img src="/sponsor-logos/superfeedr.png" alt="Superfeedr"></a></div></div>
-	<div class="logo"><div><a href="http://www.networkredux.com/"><img src="/sponsor-logos/network-redux.png" alt="Network Redux"></a></div></div>
-	<div class="logo"><div><a href="http://www.elementaltechnologies.com/"><img src="/sponsor-logos/elemental.png" alt="Elemental Technologies"></a></div></div>
-	<div class="logo"><div><a href="http://clearleft.com/"><img src="/sponsor-logos/clearleft.png" alt="ClearLeft"></a></div></div>
-	<div class="logo"><div><a href="http://status.net/"><img src="/sponsor-logos/status-net.png" alt="StatusNet"></a></div></div>
-	<div class="logo"><div><a href="http://urbanairship.com/"><img src="/sponsor-logos/urban-airship.png" alt="Urban Airship"></a></div></div>
-	<div class="logo"><div><a href="http://geoloqi.com/"><img src="/sponsor-logos/geoloqi.png" alt="Geoloqi"></a></div></div>
-	<div class="logo"><div><a href="http://tropo.com/"><img src="/sponsor-logos/tropo.png" alt="Tropo"></a></div></div>
-	<div class="logo"><div><a href="http://www.wk.com/"><img src="/sponsor-logos/wieden-kennedy.png" alt="Wieden Kennedy"></a></div></div>
-
-	<div style="clear: both;"></div>
-</div>
+<ul>
+	<li><a href="https://www.brid.gy/"><img src="/sponsor-logos/bridgy.png" alt="Bridgy"></a></li>
+	<li><a href="http://mozilla.org/"><img src="/sponsor-logos/mozilla.png" alt="Mozilla"></a></li>
+	<li><a href="http://idno.co/"><img src="/sponsor-logos/idno.png" alt="Idno"></a></li>
+	<li><a href="http://indiewebcamp.com/p3k"><img src="/sponsor-logos/p3k.png" alt="p3k"></a></li>
+	<li><a href="http://google.com"><img src="/sponsor-logos/google.png" alt="Google"></a></li>
+	<li><a href="http://superfeedr.com/"><img src="/sponsor-logos/superfeedr.png" alt="Superfeedr"></a></li>
+	<li><a href="http://www.networkredux.com/"><img src="/sponsor-logos/network-redux.png" alt="Network Redux"></a></li>
+	<li><a href="http://www.elementaltechnologies.com/"><img src="/sponsor-logos/elemental.png" alt="Elemental Technologies"></a></li>
+	<li><a href="http://clearleft.com/"><img src="/sponsor-logos/clearleft.png" alt="ClearLeft"></a></li>
+	<li><a href="http://status.net/"><img src="/sponsor-logos/status-net.png" alt="StatusNet"></a></li>
+	<li><a href="http://urbanairship.com/"><img src="/sponsor-logos/urban-airship.png" alt="Urban Airship"></a></li>
+	<li><a href="http://geoloqi.com/"><img src="/sponsor-logos/geoloqi.png" alt="Geoloqi"></a></li>
+	<li><a href="http://tropo.com/"><img src="/sponsor-logos/tropo.png" alt="Tropo"></a></li>
+	<li><a href="http://www.wk.com/"><img src="/sponsor-logos/wieden-kennedy.png" alt="Wieden Kennedy"></a></li>
+</ul>
