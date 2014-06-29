a5dce314c7d1610ca9b81143b6141a2022805f68
Peat Bakke <peat@peat.org>
2014-06-29 15:11:49 -0700

    underp?

--- skins/IndieWeb.php
+++ skins/IndieWeb.php
@@ -70,7 +70,6 @@ class IndieWebTemplate extends QuickTemplate {
     <script src="/wiki/skins/indieweb/js/vendor/modernizr.js"></script>
 
     <script type="text/javascript" src="/wiki/skins/indieweb/fragmentions.js"></script>
-    <script src="/wiki/skins/indieweb/js/vendor/jquery.js"></script>
 
     <?php print Skin::makeGlobalVariablesScript( $this->data ); ?>
 
@@ -101,6 +100,9 @@ class IndieWebTemplate extends QuickTemplate {
     <!-- Head Scripts -->
     <?php $this->html('headscripts') ?>
 
+
+    <script src="/wiki/skins/indieweb/js/foundation.min.js"></script>
+    <script src="/wiki/skins/indieweb/js/vendor/jquery.js"></script>
 
 </head>
 
@@ -370,7 +372,6 @@ class IndieWebTemplate extends QuickTemplate {
     <?php $this->html('reporttime') ?>
     </footer>
 
-<script src="/wiki/skins/indieweb/js/foundation.min.js"></script>
 <script>
     $(document).foundation();
 </script>
