b147089d36efa59abd5ec68dd527b1b74032a72e
Crystal Beasley <skinny97214@gmail.com>
2014-06-29 14:27:29 -0700

    many many css moar pretties

--- skins/IndieWeb.php
+++ skins/IndieWeb.php
@@ -206,10 +206,10 @@ class IndieWebTemplate extends QuickTemplate {
 	        <!-- end content -->
 	    
 	    </div>
-	    <div class="large-3 columns">
+	    <div class="large-3 columns side">
 	    	<h4>What is Indie?</h4>
 	    	<p>
-		    	<a href="/File:icon_4611.png" class="image"><img alt="icon 4611.png" src="http://indiewebcamp.com/images/thumb/d/d7/icon_4611.png/48px-icon_4611.png" height="48" width="48"></a><br />
+		    	<a href="/File:icon_4611.png" class="image"><img alt="icon 4611.png" src="http://indiewebcamp.com/images/thumb/d/d7/icon_4611.png/48px-icon_4611.png" height="48" width="48"></a>
 		    
 				<b>Your content is yours</b><br>When you post something on the web, it should belong to you, not a corporation. Too many companies have gone out of business and <a href="/site-deaths" title="site-deaths">lost all of their users’ data</a>. By joining the IndieWeb, your content stays yours and in your control.
 			</p>
