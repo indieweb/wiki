7f432e42c17a894fd299686c75d7404b875d889f
Aaron Parecki <aaron@parecki.com>
2014-06-30 20:19:37 -0700

    use variable for css path

--- skins/Teahouse.php
+++ skins/Teahouse.php
@@ -12,18 +12,12 @@ require_once('includes/SkinTemplate.php');
  * @package MediaWiki
  * @subpackage Skins
  */
-class SkinIndieWebTeahouse extends SkinTemplate {
-    /** Using IndieWeb */
+class SkinTeahouse extends SkinTemplate {
     function initPage( OutputPage $out ) {
         SkinTemplate::initPage( $out );
-        $this->skinname  = 'IndieWebTeahouse';
-        $this->stylename = 'indiewebTeahouse';
-        $this->template  = 'IndieWebTeahouseTemplate';
-    }
-
-    function setupSkinUserCss( OutputPage $out ){
-        parent::setupSkinUserCss( $out );
-        $out->addModuleStyles( 'skins.vector' );
+        $this->skinname  = 'Teahouse';
+        $this->stylename = 'teahouse';
+        $this->template  = 'TeahouseTemplate';
     }
 }
 
@@ -32,7 +26,7 @@ class SkinIndieWebTeahouse extends SkinTemplate {
  * @package MediaWiki
  * @subpackage Skins
  */
-class IndieWebTeahouseTemplate extends QuickTemplate {
+class TeahouseTemplate extends QuickTemplate {
     /**
      * Template filter callback for IndieWeb skin.
      * Takes an associative array of data set from a SkinTemplate-based
@@ -56,23 +50,23 @@ class IndieWebTeahouseTemplate extends QuickTemplate {
     <title><?php $this->text('pagetitle') ?></title>
 
     <meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
-    <link rel="shortcut icon" href="/wiki/skins/indieweb-teahouse/favicon.ico" />
+    <link rel="shortcut icon" href="/wiki/skins/teahouse/favicon.ico" />
     <link rel="icon" type="image/png" href="/wiki/skins/indieweb-teahouse/favicon.png" />
 
     <?php $this->html('headlinks') ?>
     
     <!-- Stylesheets -->
-
-    <link rel="stylesheet" href="/wiki/skins/indieweb-teahouse/css/normalize.css">
-    <link rel="stylesheet" href="/wiki/skins/indieweb-teahouse/css/foundation.css">
-    <link rel="stylesheet" href="/wiki/skins/indieweb-teahouse/css/main.css">
+    <?php $skinPath = $this->data['stylepath'].'/'.$this->data['stylename'] ?>
+    <link rel="stylesheet" href="<?php echo $skinPath ?>/css/normalize.css">
+    <link rel="stylesheet" href="<?php echo $skinPath ?>/css/foundation.css">
+    <link rel="stylesheet" href="<?php echo $skinPath ?>/css/main.css">
 
 	<meta property="og:title" content="<?php $this->text('pagetitle') ?>" />
-	<meta property="og:image" content="https://indiewebcamp.com/wiki/skins/indieweb-teahouse/indiewebcamp-logo-500px.png" />
+	<meta property="og:image" content="https://indiewebcamp.com<?php echo $skinPath ?>/indiewebcamp-logo-500px.png" />
 	<meta property="og:site_name" content="IndieWebCamp" />
 	<meta property="fb:admins" content="11500459,31600719,214611" />
 
-    <script src="/wiki/skins/indieweb-teahouse/js/vendor/modernizr.js"></script>
+    <script src="<?php echo $skinPath ?>/js/vendor/modernizr.js"></script>
 
     <?php print Skin::makeGlobalVariablesScript( $this->data ); ?>
 
@@ -117,7 +111,7 @@ class IndieWebTeahouseTemplate extends QuickTemplate {
     <nav class="top-bar" data-topbar>
         <ul class="title-area">
             <li class="name">
-                <a href="/"><img src="/wiki/skins/indieweb-teahouse/indiewebcamp-logo-small.png" alt="IndieWebCamp"></a>
+                <a href="/"><img src="<?php echo $skinPath ?>/indiewebcamp-logo-small.png" alt="IndieWebCamp"></a>
             </li>
         </ul>
 
@@ -351,11 +345,11 @@ class IndieWebTeahouseTemplate extends QuickTemplate {
     </footer>
 
 
-<script type="text/javascript" src="/wiki/skins/indieweb-teahouse/js/vendor/jquery.js"></script>
-<script type="text/javascript" src="/wiki/skins/indieweb-teahouse/fragmentions.js"></script>
+<script type="text/javascript" src="<?php echo $skinPath ?>/js/vendor/jquery.js"></script>
+<script type="text/javascript" src="<?php echo $skinPath ?>/fragmentions.js"></script>
 
 
-<script src="/wiki/skins/indieweb-teahouse/js/foundation.min.js"></script>
+<script src="<?php echo $skinPath ?>/js/foundation.min.js"></script>
 <script>
     $(document).foundation();
 </script>
